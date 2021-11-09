e324a3e85624c16c23b83ba451bd9359f3a18dd2
Trung Kien Huynh <trungkien.huynh@gmail.com>
2021-11-09 08:08:03 +0100

    ChartBar final tweaks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ChartBar from "./components/ChartBar";
 import "./App.css";
+import ChartBarExample from "./components/ChartBarExample";
 
 function App() {
   return (
     <React.Fragment>
       <h1>Component showcase</h1>
-      <ChartBar
-        key={0}
-        value={50}
-        maxValue={100}
-        label="x-label"
-      />
-      
+      {/* <ChartBarExample /> */}
     </React.Fragment>
   );
 }
